9e2ab11a42b91256f336c0069d3d3fc6e2ba1461
Evelynmaru <blankspace90117+github@gmail.com>
2026-08-18 16:24:17 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,7 +9,6 @@
 
 const urls = [
 	'https://forms.gle/3omidN8dwaSqRoCq8',
-	'https://forms.gle/H2XzHXstfzeSsKBF9',
 	'https://forms.gle/9cBXvByoKF1DbaDT7',
 	'https://forms.gle/pdgQq2vmTDEMeyqAA',
 	'https://forms.gle/jqe5DspLFkoFmvwt6',
